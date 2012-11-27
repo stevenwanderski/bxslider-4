e19cef612f6b4177e893fff8fc967e228c3ce079
Steven Wanderski <steven@bxcreative.com>
2012-11-27 14:17:07 +0100

    now using desandros imageloaded plugin

--- jquery.bxslider.js
+++ jquery.bxslider.js
@@ -222,7 +222,10 @@
 				if(slider.settings.controls || slider.settings.autoControls || slider.settings.pager) slider.viewport.after(slider.controls.el);
 			}
 			// preload all images, then perform final DOM / CSS modifications that depend on images being loaded
-			preloadImages(function(){
+			el.children().imagesLoaded(function(){
+				// remove the loading DOM element
+				slider.loader.remove();
+				// make el visible
 				el.css('overflow', 'visible');
 				// set the left / top position of "el"
 				setSlidePosition();
@@ -236,46 +239,6 @@
 				// if auto is true, start the show
 				if (slider.settings.auto && slider.settings.autoStart) initAuto();
 			});
-		}
-		
-		/**
-		 * Preload all images and iframes
-		 *
-		 * @param callback (function) 
-		 *  - Function to be called after images are loaded
-		 */
-		var preloadImages = function(callback){
-			// get all the images
-			var images = slider.children.find('img[src!=""], iframe');
-			// counter that stores loaded images
-			var loaded = 0;
-			// if images exist
-			if(images.length > 0){
-				// cycle through each image
-				images.each(function(index){
-					var img = $(this);
-					// on image load
-				  img.load(function(){
-						// increase the counter
-						++loaded;
-						// if all images have been loaded
-						if(images.length == loaded){
-							// remove the loading DOM element
-							slider.loader.remove();
-							// fire the callback function
-							callback();
-						}
-					});
-				// silly IE needs this hack to fire "load" events on images
-				}).each(function() {
-					if(this.complete) $(this).load();
-				});
-			// if images do not exist, fire the callback function immediately
-			}else{
-				// remove the loading DOM element
-				slider.loader.remove();
-				callback();
-			}
 		}
 		
 		/**
@@ -319,7 +282,6 @@
 					return $(this).outerHeight();
 				}).get());
 			}
-			
 			return height;
 		}
 		
@@ -1179,3 +1141,17 @@
 	}
 
 })(jQuery);
+
+/*!
+ * jQuery imagesLoaded plugin v2.1.0
+ * http://github.com/desandro/imagesloaded
+ *
+ * MIT License. by Paul Irish et al.
+ */
+
+/*jshint curly: true, eqeqeq: true, noempty: true, strict: true, undef: true, browser: true */
+/*global jQuery: false */
+
+(function(c,n){var l="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";c.fn.imagesLoaded=function(f){function m(){var b=c(i),a=c(h);d&&(h.length?d.reject(e,b,a):d.resolve(e));c.isFunction(f)&&f.call(g,e,b,a)}function j(b,a){b.src===l||-1!==c.inArray(b,k)||(k.push(b),a?h.push(b):i.push(b),c.data(b,"imagesLoaded",{isBroken:a,src:b.src}),o&&d.notifyWith(c(b),[a,e,c(i),c(h)]),e.length===k.length&&(setTimeout(m),e.unbind(".imagesLoaded")))}var g=this,d=c.isFunction(c.Deferred)?c.Deferred():
+0,o=c.isFunction(d.notify),e=g.find("img").add(g.filter("img")),k=[],i=[],h=[];c.isPlainObject(f)&&c.each(f,function(b,a){if("callback"===b)f=a;else if(d)d[b](a)});e.length?e.bind("load.imagesLoaded error.imagesLoaded",function(b){j(b.target,"error"===b.type)}).each(function(b,a){var d=a.src,e=c.data(a,"imagesLoaded");if(e&&e.src===d)j(a,e.isBroken);else if(a.complete&&a.naturalWidth!==n)j(a,0===a.naturalWidth||0===a.naturalHeight);else if(a.readyState||a.complete)a.src=l,a.src=d}):m();return d?d.promise(g):
+g}})(jQuery);
