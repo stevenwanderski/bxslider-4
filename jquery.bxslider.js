44cefe3ede66691143fa77e851296741801bc746
Jason Farrell <jason@useallfive.com>
2013-03-27 15:28:05 -0700

    Added a method to return our Slider controls element.

--- jquery.bxslider.js
+++ jquery.bxslider.js
@@ -1165,6 +1165,13 @@
 			var pagerIndex = parseInt(slider.active.index) - 1;
 			el.goToSlide(pagerIndex, 'prev');
 		}
+
+		/**
+		 * Returns our Slider controls element
+		 */
+		el.getControlsEl = function() {
+            return slider.controls.el[0];
+        }
 		
 		/**
 		 * Starts the auto show
