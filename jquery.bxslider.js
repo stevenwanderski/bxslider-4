827791089eb3371c2fab0a4d19d79b98cabcb212
Mauro E. Bender <maurobender@gmail.com>
2013-12-28 20:38:40 -0300

    Added a new method to return the current slide element.

--- jquery.bxslider.js
+++ jquery.bxslider.js
@@ -1244,6 +1244,13 @@
 		el.getCurrentSlide = function(){
 			return slider.active.index;
 		}
+		
+		/**
+		 * Returns current slide element
+		 */
+		el.getCurrentSlideElement = function(){
+			return slider.children.eq(slider.active.index);
+		}
 
 		/**
 		 * Returns number of slides in show
