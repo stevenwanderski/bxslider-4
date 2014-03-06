8d5018f082ba8b83a4fda3736995d927269f0355
Jev Zelenkov <jev.zelenkov@gmail.com>
2014-03-06 14:50:11 +0100

    updates year in copyright segment

--- jquery.bxslider.min.js
+++ jquery.bxslider.min.js
@@ -2,7 +2,7 @@
  * BxSlider v4.1.1 - Fully loaded, responsive content slider
  * http://bxslider.com
  *
- * Copyright 2013, Steven Wanderski - http://stevenwanderski.com - http://bxcreative.com
+ * Copyright 2014, Steven Wanderski - http://stevenwanderski.com - http://bxcreative.com
  * Written while drinking Belgian ales and listening to jazz
  *
  * Released under the MIT license - http://opensource.org/licenses/MIT
