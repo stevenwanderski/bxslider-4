48dd4cceb71338099b3881e3128be9580d0c3940
foo9 <fu9fu9fu9@gmail.com>
2014-09-08 01:23:27 +0900

    replace spaces to tabs

--- jquery.bxslider.js
+++ jquery.bxslider.js
@@ -277,7 +277,7 @@
 			var count = 0;
 			selector.find('img, iframe').each(function(){
                 // fix IE9 image onload bug
-                if($(this).is('img')) $(this).attr('src', $(this).attr('src'));
+				if($(this).is('img')) $(this).attr('src', $(this).attr('src'));
 				$(this).one('load', function() {
 				  if(++count == total) callback();
 				}).each(function() {
