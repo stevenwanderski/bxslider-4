67c994b1614b2d809380738c3d810351c468dd16
Mohammad Naghavi <mohamnag@gmail.com>
2014-12-23 08:55:13 +0100

    Update jquery.bxslider.js
    fixing the `updateAfterSlideTransition` not being called when there is only one slide and there will be no transitionEnd event from Browser

--- jquery.bxslider.js
+++ jquery.bxslider.js
@@ -540,14 +540,18 @@
 				// add the CSS transition-duration
 				el.css('-' + slider.cssPrefix + '-transition-duration', duration / 1000 + 's');
 				if(type == 'slide'){
-					// set the property value
-					el.css(slider.animProp, propValue);
-					// bind a callback method - executes when CSS transition completes
-					el.bind('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function(){
-						// unbind the callback
-						el.unbind('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd');
+					if(value === 0) {
 						updateAfterSlideTransition();
-					});
+					} else {
+						// set the property value
+						el.css(slider.animProp, propValue);
+						// bind a callback method - executes when CSS transition completes
+						el.bind('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function(){
+							// unbind the callback
+							el.unbind('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd');
+							updateAfterSlideTransition();
+						});
+					}
 				}else if(type == 'reset'){
 					el.css(slider.animProp, propValue);
 				}else if(type == 'ticker'){
