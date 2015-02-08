4f56d19669d309a33a5c31e33aae706b5b270cdb
Eric Wafford <ewafford@tidalwaveagency.com>
2015-02-07 21:48:28 -0600

    Accessing bxslider's slider object inside its “onSliderLoad” callback returns undefined #475

--- src/js/jquery.bxslider.js
+++ src/js/jquery.bxslider.js
@@ -313,7 +313,7 @@
 			// make sure everything is positioned just right (same as a window resize)
 			el.redrawSlider();
 			// onSliderLoad callback
-			slider.settings.onSliderLoad(slider.active.index);
+			slider.settings.onSliderLoad(slider,slider.active.index);
 			// slider has been fully initialized
 			slider.initialized = true;
 			// bind the resize call to the window
