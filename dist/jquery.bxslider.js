9a823bd476e2f91b6c9f672b431207225add1209
Huseyin Sekmenoglu <hus_as@yahoo.com>
2017-03-26 00:12:30 +0300

    fix merge conflicts

--- dist/jquery.bxslider.js
+++ dist/jquery.bxslider.js
@@ -476,7 +476,7 @@
             breakPoint = counter + getNumberSlidesShowing();
             counter += slider.settings.moveSlides <= getNumberSlidesShowing() ? slider.settings.moveSlides : getNumberSlidesShowing();
           }
-	  return counter;
+		  return counter;
         }
       // if moveSlides is 0 (auto) divide children length by sides showing, then round up
       } else {
