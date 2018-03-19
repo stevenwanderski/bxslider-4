9daaac4865dc0e230b95919c083b64078035589e
danila-dev <prog1@localhost.localdomain>
2018-03-19 13:25:43 +0300

    first fix for firefox 59

--- src/js/jquery.bxslider.js
+++ src/js/jquery.bxslider.js
@@ -1106,7 +1106,7 @@
         slider.touch.start.x = touchPoints[0].pageX;
         slider.touch.start.y = touchPoints[0].pageY;
 
-        if (slider.viewport.get(0).setPointerCapture) {
+        if (slider.viewport.get(0).setPointerCapture && orig.pointerId > 0) {
           slider.pointerId = orig.pointerId;
           slider.viewport.get(0).setPointerCapture(slider.pointerId);
         }
